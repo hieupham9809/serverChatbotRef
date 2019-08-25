8c523ddf99a089f7cf9f85cb27eb2514e9c67b3a
caochanhduong <caochanhduongbku@gmail.com>
2019-08-25 20:06:28 +0700

    strip pymongo

--- api_conversation_manager.py
+++ api_conversation_manager.py
@@ -9,7 +9,7 @@ import time
 import random
 import numpy as np
 from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app) 
