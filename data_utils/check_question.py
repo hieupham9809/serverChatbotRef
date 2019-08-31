1a2d18fcdc03e5781e153c8e0d42dceba228a46b
caochanhduong <caochanhduongbku@gmail.com>
2019-08-31 12:35:53 +0700

    fix api message classify

--- data_utils/check_question.py
+++ data_utils/check_question.py
@@ -1,9 +1,12 @@
 def check_question(message):
-    list_question_signal=["nhỉ","ai","ở đâu","đi đâu","bao giờ","khi nào","lúc nào","hồi nào","vì sao","tại sao","làm sao","thế nào","gì","bao nhiêu","mấy","?"]
+    list_question_signal=["nhỉ"," ai ","ở đâu","đi đâu","bao giờ","khi nào","lúc nào","hồi nào","vì sao","tại sao","làm sao","thế nào","gì","bao nhiêu","mấy","?"]
     for signal in list_question_signal:
         if signal in message.lower():
+            print(signal)
             return True
     if message.split(" ")[-1].lower()=="chưa":
         return True
     return False
+
+# check_question("hieupham989@gmail.com nha, số điện thoại là 0961508884")
 
