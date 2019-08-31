fe36d3d20af367c20ba9e11b22cdd5e7c531fba9
caochanhduong <caochanhduongbku@gmail.com>
2019-08-31 23:02:03 +0700

    fix api classify question

--- data_utils/check_question.py
+++ data_utils/check_question.py
@@ -1,12 +1,22 @@
 def check_question(message):
-    list_question_signal=["thế","nhỉ"," ai ","ở đâu","đi đâu","bao giờ","khi nào","lúc nào","hồi nào","vì sao","tại sao","làm sao","thế nào","gì","bao nhiêu","mấy","?"]
+    #bắt WH question 
+    list_question_signal=["thế","nhỉ"," ai"," ai ","ở đâu","ở mô","đi đâu","bao giờ","bao lâu","khi nào","lúc nào","hồi nào","vì sao","tại sao","làm sao","như nào","thế nào","cái chi","gì","bao nhiêu","mấy","?"]
     for signal in list_question_signal:
         if signal in message.lower():
             print(signal)
             return True
-    if message.split(" ")[-1].lower()=="chưa" or message.split(" ")[-1].lower()=="vậy":
+    #bắt YES-NO/WH question mà signal cuối câu 
+    if message.split(" ")[-1].lower()=="chưa" or message.split(" ")[-1].lower()=="vậy" or message.split(" ")[-1].lower()=="không" or message.split(" ")[-1].lower()=="ta" or message.split(" ")[-1].lower()=="sao":
         return True
+
+    #bắt YES-NO question cuối câu có chủ ngữ
+    list_question_signal_last=["vậy","chưa","không","sao"]
+    list_subject=["bạn","cậu","ad","anh","chị"]
+    for subject in list_subject:
+        for question_signal_last in list_question_signal_last:
+            if message.split(" ")[-1].lower()==subject and message.split(" ")[-2].lower()==question_signal_last:
+                return True 
     return False
 
-# check_question("hieupham989@gmail.com nha, số điện thoại là 0961508884")
+# print(check_question("sinh viên khoa máy tính được đi không bạn"))
 
