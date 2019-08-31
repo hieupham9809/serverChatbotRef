8ec7595c16c5dc6cd64fa90b2a625944895c8cbc
caochanhduong <caochanhduongbku@gmail.com>
2019-08-31 12:56:06 +0700

    fix api classify

--- data_utils/check_question.py
+++ data_utils/check_question.py
@@ -1,10 +1,10 @@
 def check_question(message):
-    list_question_signal=["nhỉ"," ai ","ở đâu","đi đâu","bao giờ","khi nào","lúc nào","hồi nào","vì sao","tại sao","làm sao","thế nào","gì","bao nhiêu","mấy","?"]
+    list_question_signal=["thế","nhỉ"," ai ","ở đâu","đi đâu","bao giờ","khi nào","lúc nào","hồi nào","vì sao","tại sao","làm sao","thế nào","gì","bao nhiêu","mấy","?"]
     for signal in list_question_signal:
         if signal in message.lower():
             print(signal)
             return True
-    if message.split(" ")[-1].lower()=="chưa":
+    if message.split(" ")[-1].lower()=="chưa" or message.split(" ")[-1].lower()=="vậy":
         return True
     return False
 
